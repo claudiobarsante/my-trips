5d70bcd91c7e8c573b44a0bbfd2d21c3248e8b17
Claudio Barsante <clbmribas@gmail.com>
2021-07-06 15:30:32 -0300

    feat(<AboutPage/>)

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,8 +1,9 @@
-import { GraphQLClient, gql } from 'graphql-request';
+import { GraphQLClient } from 'graphql-request';
 
 const endpoint = process.env.GRAPHQL_HOST || '';
 // 'https://api-ca-central-1.graphcms.com/v2/cko4swhyxrrka01z12h26hacf/master';
 
+console.log('token', process.env.GRAPHQL_TOKEN);
 const client = new GraphQLClient(endpoint, {
   headers: {
     authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,43 @@
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries';
+import { GetStaticProps } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import AboutPageTemplate, { AboutPageTemplateProps } from 'templates/Pages';
+import client from '../graphql/client';
+
+export default function AboutPage({ heading, body }: AboutPageTemplateProps) {
+  const router = useRouter();
+
+  // -- it could return a loading component...anything until it's been created
+  if (router.isFallback) return null;
+
+  return <AboutPageTemplate heading={heading} body={body} />;
+}
+
+/*
+getStaticPaths => generate urls in build time
+getStaticProps => get data to the page (props) in build time - static
+getServerSideProps => get data to the page (props) in runtime - every request
+*/
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 });
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }));
+
+  return { paths, fallback: true };
+}
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  });
+
+  if (!page) return { notFound: true };
+
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  };
+};
