418a387d33df78159310a9ea581bebfee923abaf
Claudio Barsante <clbmribas@gmail.com>
2021-07-12 11:58:36 -0300

    feat(Places)

--- src/pages/place/[slug].tsx
+++ src/pages/place/[slug].tsx
@@ -1,17 +1,19 @@
-import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql';
-import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries';
+import { GetPlaceBySlugQuery, GetPlacesQuery } from 'graphql/generated/graphql';
+import { GET_PLACES, GET_PLACE_BY_SLUG } from 'graphql/queries';
 import { GetStaticProps } from 'next';
 import { useRouter } from 'next/dist/client/router';
-import AboutPageTemplate, { AboutPageTemplateProps } from 'templates/Pages';
-import client from '../graphql/client';
 
-export default function AboutPage({ heading, body }: AboutPageTemplateProps) {
+import client from 'graphql/client';
+import React from 'react';
+import PlacesTemplate, { PlacesTemplateProps } from 'templates/Places';
+
+export default function Place({ place }: PlacesTemplateProps) {
   const router = useRouter();
 
   // -- it could return a loading component...anything until it's been created
   if (router.isFallback) return null;
 
-  return <AboutPageTemplate heading={heading} body={body} />;
+  return <PlacesTemplate place={place} />;
 }
 
 /*
@@ -24,27 +26,29 @@ getServerSideProps => get data to the page (props) in runtime - every request
 To generate queries types install GraphQL Code Generator and follow the steps
 */
 export async function getStaticPaths() {
-  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, {
     first: 3
   });
 
-  const paths = pages.map(({ slug }) => ({
+  const paths = places.map(({ slug }) => ({
     params: { slug }
   }));
 
   return { paths, fallback: true };
 }
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
-    slug: `${params?.slug}`
-  });
+  const { place } = await client.request<GetPlaceBySlugQuery>(
+    GET_PLACE_BY_SLUG,
+    {
+      slug: `${params?.slug}`
+    }
+  );
 
-  if (!page) return { notFound: true };
+  if (!place) return { notFound: true };
 
   return {
     props: {
-      heading: page.heading,
-      body: page.body.html
+      place
     }
   };
 };
