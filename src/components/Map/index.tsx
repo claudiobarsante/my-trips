7df6be21f46170a26bbe7a94b1cf6f462a98730c
Claudio Barsante <clbmribas@gmail.com>
2021-07-12 18:08:43 -0300

    feat(next-seo)

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -16,6 +16,25 @@ export type MapProps = {
   places?: Place[];
 };
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
+const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID;
+const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID;
+
+//todo -- ajust mapbox custom style
+// const CustomTileLayer = () => {
+//   return MAPBOX_API_KEY ? (
+//     <TileLayer
+//       attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//       url={`https://api.mapbox.com/styles/v1/clbmribas/ckr0wy5h026hr19qmhxh9eblo.html?fresh=true&title=view&access_token=pk.eyJ1IjoiY2xibXJpYmFzIiwiYSI6ImNrZzg5NW50YTA4bXQyenFyN216b253cnQifQ.9qVmdpre3iZKlKuA-x7wzQ`}
+//     />
+//   ) : (
+//     <TileLayer
+//       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//     />
+//   );
+// };
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter();
   return (
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
 import Head from 'next/head';
 import GlobalStyles from 'styles/global';
+import NextNprogress from 'nextjs-progressbar';
+// -- SEO
+import { DefaultSeo } from 'next-seo';
+import SEO from '../../next-seo.config'; // import your default seo configuration
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -17,12 +20,16 @@ function App({ Component, pageProps }: AppProps) {
           crossOrigin=""
         />
         <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project to show my favorite spots in the world"
-        />
       </Head>
+      <DefaultSeo {...SEO} />
       <GlobalStyles />
+      <NextNprogress
+        color="#f231a5"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </>
   );
